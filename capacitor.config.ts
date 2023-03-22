34c66d8cfbd1ab85aab5a549f64ea4cb83861faa
jjmmarcos <javierbulevar8@gmail.com>
2023-03-22 19:51:37 +0100

    added ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.javier.rpsGame',
+  appName: 'rpsGame',
+  webDir: 'dist/rps-game',
+  bundledWebRuntime: false
+};
+
+export default config;
