90ebd120d9ec1616a26f2cb1fa77081fc91589c7
jjmmarcos <javierbulevar8@gmail.com>
2023-03-26 12:21:31 +0200

    stadistics component developed

--- src/app/pages/statistics/statistics.component.ts
+++ src/app/pages/statistics/statistics.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import Chart from 'chart.js/auto'
+import { rpsGameResult } from 'src/app/interfaces/rpsServerResponse.interface';
+import { RpsGameService } from 'src/app/services/RpsGameService.service';
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
+  gameSaved: rpsGameResult[] = [];
+  gameStadistics: number[] = [];
+  constructor(private rpsGameService: RpsGameService) {
+    this.gameSaved = JSON.parse( localStorage.getItem('rpsGame')! ) || [];
+  }
 
-  constructor() { }
+  ngOnInit(){
+    console.log(this.gameSaved);
+    this.gameStadistics = [this.countOcurrences('You win!'), this.countOcurrences('You lose!'), this.countOcurrences('Tie!')];
+    this.showStadistics();
+  }
 
-  ngOnInit() {
+  countOcurrences(value: string): number {
+    let count = 0;
+    this.gameSaved.filter((v) => (v.rpsServerResponse.gameResult == value && count++));
+    return count;
+  }
+
+  showStadistics() {
+    var ctx = (<any>document.getElementById('stadistics-chart')).getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+        labels: ["Win", "Lose", "Tie"],
+        datasets: [{
+              label: "Number of times",
+              backgroundColor: [
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                
+              ],
+              borderColor: [
+                'rgba(75, 192, 192, 1)',
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)'                
+              ],
+              data: this.gameStadistics,
+              borderWidth: 1
+        }]
+       }
+    });
   }
 
 }
